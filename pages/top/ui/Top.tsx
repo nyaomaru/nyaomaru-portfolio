3fd800b746640a228c47818b8e6c274bdf977d6c
nyaomaru <nyaonyao0725@gmail.com>
2026-08-10 15:40:02 +0200

    fix: terminal margin

--- pages/top/ui/Top.tsx
+++ pages/top/ui/Top.tsx
@@ -1,9 +1,9 @@
 import { Terminal } from '@/widgets/terminal';
 
 const TOP_PAGE_CLASS_NAME =
-  'h-[100dvh] w-full flex flex-col items-center justify-center p-4 sm:pt-52';
+  'h-[100dvh] w-full grid grid-rows-[minmax(1rem,1fr)_auto_minmax(1rem,1fr)] items-center justify-items-center px-4 sm:grid-rows-[minmax(13rem,1fr)_auto_minmax(1rem,1fr)]';
 const TERMINAL_WRAPPER_CLASS_NAME =
-  'w-full flex justify-center max-w-[calc(75dvh-10.5rem)] sm:max-w-[calc(177.7778dvh-24.8889rem)]';
+  'row-start-2 w-full flex justify-center max-w-[calc(75dvh-10.5rem)] sm:max-w-[calc(177.7778dvh-24.8889rem)]';
 
 const Top = () => {
   return (
